f4207198be27fb09f6360dbe0eaebf062178e8c6
MartinKalema <kalemaaaaaaaa@gmail.com>
2025-01-17 13:07:49 +0300

    finally done the call backs stage.

--- src/ChickenDiseaseClassifier/config/configuration.py
+++ src/ChickenDiseaseClassifier/config/configuration.py
@@ -1,6 +1,7 @@
+import os
 from ChickenDiseaseClassifier.constants import *
 from ChickenDiseaseClassifier.utils.common import create_directories, read_yaml, get_size
-from ChickenDiseaseClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig)
+from ChickenDiseaseClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig)
 
 
 class ConfigurationManger:
@@ -45,6 +46,23 @@ class ConfigurationManger:
         )
 
         return prepare_base_model_config
+    
+
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+
+        return prepare_callback_config
 
         
 
